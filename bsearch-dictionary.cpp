afa45228fac46ae2628b94f038dbe2f027f01134
Parshwa D Shah <shah334@purdue.edu>
2017-07-16 03:01:49 -0400

    Commit search engine

--- bsearch-dictionary.cpp
+++ bsearch-dictionary.cpp
@@ -128,7 +128,7 @@ BinarySearchDictionary::sort()
 
 	printf("After\n");
 	for (int i = 0; i < currentNumber; i++) {
-		array[i] = heap[i];
+		array[i] = removeMin();
 		printf("%s\n", array[i].key);
 	}
 }
