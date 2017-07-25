12d9906e57c1b7788c85b6696b1a3caf9ae85e3c
Parshwa D Shah <shah334@purdue.edu>
2017-07-24 20:58:43 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -38,8 +38,8 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
   string s;
   string w = "";
   while(getline(f,s)){
-	  printf("%s",s.c_str());
-	  printf("\n\n");
+	  //printf("%s",s.c_str());
+	  //printf("\n\n");
 	  for(int i=0;i<s.length();i++){
 		  if(s[i]!=' '){
 			  w = w + s[i];
@@ -51,7 +51,6 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
 		  }
 
 	  }
-	  
   }
   
 
